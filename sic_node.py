6590849865d2b2ddac1d9fd375a2c84fa4b5eb8a
דור שפירא <Dor.Shapira@outlook.com>
2023-08-03 01:08:58 +0300

    added CUnaryOpKind enum

--- sic_node.py
+++ sic_node.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import enum
 import sic_token as tk
 import typing
 
@@ -16,6 +17,22 @@ class StringLiteral:
 class ConstantLiteral:
     def __init__(self, token: tk.Token):
         self.token = token
+
+
+class CUnaryOpKind(enum.Enum):
+    """"""
+    # https://www.scaler.com/topics/pre-increment-and-post-increment-in-c/
+    # increase/increase then return
+    PreIncrease = enum.auto()
+    PreDecrease = enum.auto()
+    # return then increase/increase
+    PostIncrease = enum.auto()
+    PostDecrease = enum.auto()
+    Plus = enum.auto()  # '+'
+    Minus = enum.auto()  # '-'
+    BitwiseNOT = enum.auto()  # '~'
+    LogicalNOT = enum.auto()  # '!'
+    Sizeof = enum.auto()  # 'sizeof'
 
 
 Node = typing.Union[Identifier, StringLiteral, ConstantLiteral]
