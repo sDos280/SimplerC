5169281285bf327370a145e6cc8c7e73d957a6c4
דור שפירא <Dor.Shapira@outlook.com>
2023-08-08 11:53:37 +0300

    added peek_type_specifier and is_token_type_specifier, and started working on peek_cast_expression and peek_type_name

--- sic_dry_parser.py
+++ sic_dry_parser.py
@@ -22,6 +22,10 @@ class DryParser:
         self.index += 1
         self.current_token = self.lexer.tokens[self.index]
 
+    def drop_token(self):
+        self.index -= 1
+        self.current_token = self.lexer.tokens[self.index]
+
     def is_token_kind(self, kind: tk.TokenKind | list[tk.TokenKind]) -> bool:
         if isinstance(kind, list):
             return self.current_token.kind in kind
@@ -40,6 +44,9 @@ class DryParser:
             tk.TokenKind.SIGNED,
             tk.TokenKind.UNSIGNED
         ])
+
+    def is_token_type_specifier(self) -> bool:
+        return self.is_token_type_name()
 
     @staticmethod
     def is_node(node_type: typing.Type[node.Node] | list[typing.Type[node.Node]], obj: node.Node) -> bool:
@@ -155,7 +162,23 @@ class DryParser:
         self.fetal_token("unary operator expected")
 
     def peek_cast_expression(self):
-        pass
+        if self.is_token_kind(tk.TokenKind.OPENING_PARENTHESIS):
+            self.peek_token()  # peek ( token
+
+            if self.is_token_type_name():
+                type_name: node.TypeName = self.peek_type_name()
+
+                self.peek_token()  # peek ) token
+
+                cast_expression: node.Node = self.peek_cast_expression()
+
+                return node.CCast(type_name, cast_expression)
+            else:
+                self.drop_token()  # drop ( token
+
+        unary_expression: node.Node = self.peek_unary_expression()
+
+        return unary_expression
 
     def peek_multiplicative_expression(self):
         pass
@@ -214,8 +237,44 @@ class DryParser:
     def peek_init_declarator(self):
         pass
 
-    def peek_type_specifier(self):
-        pass
+    def peek_type_specifier(self) -> node.CTypeSpecifier:
+        match self.current_token.kind:
+            case tk.TokenKind.VOID:
+                self.peek_token()  # peek void token
+
+                return node.CTypeSpecifier.VOID
+            case tk.TokenKind.CHAR:
+                self.peek_token()  # peek char token
+
+                return node.CTypeSpecifier.CHAR
+            case tk.TokenKind.SHORT:
+                self.peek_token()  # peek short token
+
+                return node.CTypeSpecifier.SHORT
+            case tk.TokenKind.INT:
+                self.peek_token()  # peek int token
+
+                return node.CTypeSpecifier.INT
+            case tk.TokenKind.LONG:
+                self.peek_token()  # peek long token
+
+                return node.CTypeSpecifier.LONG
+            case tk.TokenKind.FLOAT:
+                self.peek_token()  # peek float token
+
+                return node.CTypeSpecifier.FLOAT
+            case tk.TokenKind.DOUBLE:
+                self.peek_token()  # peek double token
+
+                return node.CTypeSpecifier.DOUBLE
+            case tk.TokenKind.SIGNED:
+                self.peek_token()  # peek signed token
+
+                return node.CTypeSpecifier.SIGNED
+            case tk.TokenKind.UNSIGNED:
+                self.peek_token()  # peek unsigned token
+
+                return node.CTypeSpecifier.UNSIGNED
 
     def peek_struct_or_union_specifier(self):
         pass
@@ -274,8 +333,26 @@ class DryParser:
     def peek_identifier_list(self):
         pass
 
-    def peek_type_name(self):
-        pass
+    def peek_type_name(self) -> node.TypeName:
+        specifier_counter: node.CTypeSpecifier = node.CTypeSpecifier(0)
+        c_primary_type: node.CPrimaryType = node.CPrimaryType.VOID
+
+        while self.is_token_type_specifier():
+            specifier: node.CTypeSpecifier = self.peek_type_specifier()
+            if specifier == node.CTypeSpecifier.UNSIGNED or specifier == node.CTypeSpecifier.SIGNED:
+                specifier_counter |= specifier
+            else:
+                specifier_counter += specifier
+
+            c_primary_type = node.c_type_specifier_counter_to_c_primary_type.get(specifier_counter)
+
+            if c_primary_type is None:
+                self.fetal_token("invalid type specifier")
+
+        if specifier_counter == 0:
+            self.fetal_token("type specifier expected")
+
+        return c_primary_type
 
     def peek_abstract_declarator(self):
         pass
