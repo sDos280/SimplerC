caf4918ca0bf9c84c9a5b671407cc6ce1abefe77
דור שפירא <Dor.Shapira@outlook.com>
2023-08-11 11:30:45 +0300

    peek_assignment_operator meant to be the peek_binary_assignment_operator function

--- sic_dry_parser.py
+++ sic_dry_parser.py
@@ -119,7 +119,7 @@ class DryParser:
         else:
             self.fatal_token("Expected assignment operator token")
 
-    def peek_binary_assignment_operator(self) -> node.CBinaryOpKind:
+    def peek_assignment_operator(self) -> node.CBinaryOpKind:
         if self.is_token_kind(tk.TokenKind.EQUALS):
             self.peek_token()  # peek the = token
             return node.CBinaryOpKind.Assignment
@@ -493,19 +493,7 @@ class DryParser:
         conditional_expression: node.Node = self.peek_conditional_expression()
 
         if self.is_assignment_operator():
-            binary_assignment_op: node.CBinaryOpKind = self.peek_binary_assignment_operator()  # peek assignment operator token
-
-            sub_assignment_expression: node.Node = self.peek_assignment_expression()
-
-            return node.CBinaryOp(binary_assignment_op, conditional_expression, sub_assignment_expression)
-        else:
-            return conditional_expression
-
-    def peek_assignment_operator(self) -> node.Node:
-        conditional_expression: node.Node = self.peek_conditional_expression()
-
-        if self.is_assignment_operator():
-            binary_assignment_op: node.CBinaryOpKind = self.peek_binary_assignment_operator()  # peek assignment operator token
+            binary_assignment_op: node.CBinaryOpKind = self.peek_assignment_operator()  # peek assignment operator token
 
             sub_assignment_expression: node.Node = self.peek_assignment_expression()
 
