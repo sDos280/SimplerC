3704c74f8b42e09289fba3f2d30844d6491068c5
דור שפירא <Dor.Shapira@outlook.com>
2023-09-27 12:20:17 +0300

    add IrScope, SicScope and emit_external_declaration

--- sic_emitter.py
+++ sic_emitter.py
@@ -3,3 +3,42 @@
 the emitter job is to emit the ast code
 
 """
+import sic_node as node
+import llvmlite.ir as ir
+
+
+class IrScope:
+    def __init__(self):
+        self.current_function: ir.Function | None = None
+        self.current_block: ir.Block | None = None
+
+
+class SicScope:
+    def __init__(self):
+        self.current_function: node.FunctionDefinition | None = None
+        self.current_block: node.CompoundStatement | None = None  # a block in SimpleC is a compound statement
+        self.current_while: node.While | None = None
+        self.current_for: node.For | None = None
+        self.current_if: node.If | None = None
+
+
+class Emitter:
+    def __init__(self, lexer, translation_unit):
+        self.lexer = lexer
+        self.translation_unit = translation_unit
+        self.ir_scope = IrScope()
+        self.sic_scope = SicScope()
+
+        self.module = ir.Module()
+
+    def emit_translation_unit(self):
+        for external_declaration in self.translation_unit:
+            self.emit_external_declaration(external_declaration)
+
+    def emit_external_declaration(self, external_declaration: node.ExternalDeclaration):
+        if isinstance(external_declaration, node.FunctionDefinition):
+            self.emit_function_definition(external_declaration)
+        elif isinstance(external_declaration, node.Declaration):
+            assert False, "not implemented"
+        else:
+            assert False, "unknown external declaration"
