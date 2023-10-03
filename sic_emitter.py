80e7055198b90c3d24c203c1c9c9c416c0267c90
דור שפירא <Dor.Shapira@outlook.com>
2023-10-03 23:35:43 +0300

    add emit_if_statement

--- sic_emitter.py
+++ sic_emitter.py
@@ -31,6 +31,8 @@ class Emitter:
 
         self.module = ir.Module()
 
+        self.cfb: ir.IRBuilder | None = None  # current function builder
+
         self.identifiers_table: dict[str, ...] = {}
 
     # -------------------------------------------------------
@@ -56,37 +58,34 @@ class Emitter:
     # emit functions
     def emit_translation_unit(self):
         for external_declaration in self.translation_unit:
-            self.emit_external_declaration(external_declaration)
+            if isinstance(external_declaration, node.FunctionDefinition):
+                self.emit_function_definition(external_declaration)
+            elif isinstance(external_declaration, node.Declaration):
+                self.emit_declaration(external_declaration)
 
-    def emit_external_declaration(self, external_declaration: node.ExternalDeclaration):
-        if isinstance(external_declaration, node.FunctionDefinition):
-            function_ir = self.emit_function_definition(external_declaration)
-        elif isinstance(external_declaration, node.Declaration):
-            assert False, "not implemented"
-        else:
-            assert False, "unknown external declaration"
+    def emit_if_statement(self, if_statement: node.If):
+        # create new if block
+        if_block: ir.Block = self.cfb.append_basic_block()
 
-    def emit_function_definition(self, function_definition: node.FunctionDefinition) -> ir.Function:
-        function_type = self.emit_function_type(function_definition)
-        function_ir = ir.Function(self.module, function_type, name=function_definition.identifier.token.string)
-        for parameter_name, parameter_ir in zip(function_definition.parameters_declaration, function_ir.args):
-            parameter_ir.name = parameter_name.identifier.token.string
-            self.identifiers_table[parameter_name.identifier.token.string] = parameter_ir
+        with self.cfb.goto_block(if_block):
+            ir_condition = self.emit_expression(if_statement.condition)
 
-        self.ir_scope.current_function = function_ir
-        self.sic_scope.current_function = function_definition
-        self.emit_function_body(function_definition)
+            if isinstance(if_statement.else_body, node.NoneNode):
+                # there is no else body
+                with self.cfb.if_then(ir_condition):
+                    self.add_statement(if_statement.body)
+            else:
+                # there is an else body
+                with self.cfb.if_else(ir_condition) as (then, otherwise):
+                    with then:
+                        self.add_statement(if_statement.body)
+                    with otherwise:
+                        self.add_statement(if_statement.else_body)
 
-        # clear the scope
-        for parameter_name in function_definition.parameters_declaration:
-            del self.identifiers_table[parameter_name.identifier.token.string]
+        return if_block
+    # -------------------------------------------------------
 
-        self.ir_scope.current_function = None
-        self.sic_scope.current_function = None
-        return function_ir
+    # -------------------------------------------------------
+    # add functions
 
-    def emit_function_type(self, function_definition: node.FunctionDefinition) -> ir.FunctionType:
-        return_type_ir = self.sic_type_to_ir_type(function_definition.type_name)
-        parameter_types_ir = [self.sic_type_to_ir_type(parameter_declaration.type_name) for parameter_declaration in function_definition.parameters_declaration]
-
-        return ir.FunctionType(return_type_ir, parameter_types_ir)
+    # -------------------------------------------------------
