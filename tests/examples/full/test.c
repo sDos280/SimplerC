3297875e84f8df067b1f806e88faab971a80fd93
דור שפירא <Dor.Shapira@outlook.com>
2023-10-08 14:58:06 +0300

    update full example

--- tests/examples/full/test.c
+++ tests/examples/full/test.c
@@ -1,44 +1,7 @@
 func int fibonacci( int n ){
-	int i = n;
-	int iii = i;
-
 	if (n <= 1){
 		return 1;
 	}
 
-
-
     return fibonacci( n - 1 ) + fibonacci( n - 2);
 }
-
-
-/*func int fibonacci(int n);
-func int wwtt(int n);
-
-func int fibonacci(int n) {
-	if (n <= 1) {
-		return n;
-	}
-	return fibonacci(n - 1) + fibonacci(n - 2);
-}*/
-
-/*int car = 5;
-float cdr = (float)(6) + car + fddfd;*/
-
-/*func int isPrime(int num) {
-	int i;
-
-    if (num <= 1) {
-        return 0;
-    }
-
-    for (i = 2; i * i <= num; i++) {
-        if (num % i == 0) {
-            float pi = 3.14;
-            return 0;
-        }
-    }
-
-    prunt(sizeof (int),66);
-    return 1;
-}*/
--- tests/examples/full/test.py
+++ tests/examples/full/test.py
@@ -2,6 +2,8 @@ import sic_lexer
 import sic_dry_parser
 import sic_ast_visitor
 import sic_emitter
+import sic_compiler_front_hand
+from ctypes import CFUNCTYPE, c_int
 import json
 
 with open('test.c', 'r') as f:
@@ -16,12 +18,21 @@ translation_unit = parser.peek_translation_unit()
     [external_declaration.to_dict() for external_declaration in translation_unit],
     indent=2), end='\n\n')"""
 
-
 ast_visitor = sic_ast_visitor.ASTVisitor(lexer, translation_unit)
 
 ast_visitor.visit_translation_unit()
 
 emitter = sic_emitter.Emitter(lexer, translation_unit)
-emitter.emit_translation_unit()
+llvm_ir = emitter.emit_translation_unit()
+print(llvm_ir)
 
-print()
+engine = sic_compiler_front_hand.create_execution_engine()
+mod = sic_compiler_front_hand.compile_ir(engine, llvm_ir)
+
+# Look up the function pointer (a Python int)
+func_ptr = engine.get_function_address("fibonacci")
+
+# Run the function via ctypes
+cfunc = CFUNCTYPE(c_int)(func_ptr)
+for i in range(10):
+    print(f"cfunc({i}) = {cfunc(i)}")
